833c4f85104231f18eb928c6bd6edbfa0590ab5d
Gustavo rodrigues <gustavinhorodriguessoares@gmail.com>
2026-05-26 09:15:16 -0300

    aula 6

---
+++ Aula 06/script.js
@@ -0,0 +1,31 @@
+let aluno = "Gustavo"
+var aluno_var = "Lucas"
+const nome = "Pietro"
+
+console.log(aluno_var)
+
+aluno = "Vinicios"
+
+console.log(aluno_var) 
+
+let fruta1 = "uva"
+let fruta2 = "maça"
+let fruta3 = "manga"
+
+console.log(fruta1, fruta2, fruta3)
+
+let frutas =["maçã", "uva", "manga"]
+
+console.log(frutas.length) 
+
+
+let cadastro = {
+    id: 1, 
+    nome: "gustavo",
+    email: "teste@email.com",
+    senha: 123
+}
+
+console.log(cadastro)
+
+
