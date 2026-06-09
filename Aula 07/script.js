ed964d0be12cb7834805b69d909730b3b2881353
Gustavo Rodrigues <gustavinhorodriguessoares@gmail.com>
2026-06-09 09:10:45 -0300

    Aula 08

--- Aula 07/script.js
+++ Aula 07/script.js
@@ -1,3 +1,4 @@
+let resultado = document.querySelector('#resultado')
 
 function exibirNome (){
     let nome = document.querySelector('#nome').value
@@ -8,23 +9,23 @@ function exibirNome (){
 function somar(){
     let valA = Number(document.querySelector('#valorA').value)
     let valB = Number(document.querySelector('#valorB').value)
-    alert(valA + valB)
+resultado.innerHTML = valA + valB
 }
 
 function subtrair(){
     let valA = Number(document.querySelector('#valorC').value)
     let valB = Number(document.querySelector('#valorD').value)
-    alert(valA - valB)
+    resultado.innerHTML = valA - valB
 }
 
 function divisão(){
     let valA = Number(document.querySelector('#valorE').value)
     let valB = Number(document.querySelector('#valorF').value)
-    alert(valA / valB)
+    resultado.innerHTML = valA / valB
 }
 
 function multiplicação(){
     let valA = Number(document.querySelector('#valorG').value)
     let valB = Number(document.querySelector('#valorH').value)
-    alert(valA * valB)
+    resultado.innerHTML = valA * valB
 }
